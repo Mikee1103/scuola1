8ee44dad38dafdcb7538b03ac695562c5a4b2498
Mikee1103 <160124042+Mikee1103@users.noreply.github.com>
2024-02-16 13:54:00 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,79 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Main{
+    public static void main (String [] args){
+        Scanner in = new Scanner(System.in);
+        Random gen= new Random();
+        int lungh=100;
+        int continua;
+        boolean esci=false;
+        int dim=0;
+        int pos;
+        int conta=0;
+        int n;
+        int scelta;
+        int [] numeri = new int [100];
+        do{
+            System.out.println("Scegli un'opzione:");
+            System.out.println("1.caricamento tastiera");
+            System.out.println("2.caricamento random");
+            System.out.println("3.visualizzazione");
+            System.out.println("4.inserimento in posizione");
+            System.out.println("5.cancellazione di un elemento cercato");
+            System.out.println("6.ESCI");
+            scelta= in.nextInt();
+            switch(scelta) {
+                case 1:
+                    for (int i = 0; i<100;i++) {
+                        System.out.println("Inserisci il numero");
+                        n = in.nextInt();
+                        numeri[i] = n;
+                        dim=dim+1;
+                        lungh=lungh-1;
+                        System.out.println("vuoi continuare? (si=1/no=0");
+                        continua=in.nextInt();
+                        if (continua == 0)
+                            break;
+
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < 100; i++) {
+                        numeri[i] = gen.nextInt(50);
+                    }
+                    System.out.println("array casuale completato");
+                    break;
+                case 3:
+                    for (int o = 0; o < dim ; o++) {
+                        System.out.print(numeri[o]+", ");
+                    }
+                    break;
+                case 4:
+                    System.out.println("in che posizione vuoi inserire il numero?");
+                    pos= in.nextInt();
+                    if(pos<dim) {
+                        System.out.println("Inserisci il numero");
+                        n = in.nextInt();
+                        numeri[pos] = n;
+                    }
+                    if(pos==dim)
+                        dim=dim+1;
+                    break;
+                case 5:
+                    System.out.println("che posizione dell'array vuoi cancellare?");
+                    pos=in.nextInt();
+
+                case 6:
+                    esci=true;
+                    break;
+            }
+
+
+
+
+
+
+            }while(esci==false);
+
+        }
+    }
